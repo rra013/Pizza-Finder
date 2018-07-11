642a106d64cea384bfa6a9002f271deeae2f4876
Rishi Anand <rishi@rishis-macbook-pro.local>
2018-07-11 13:38:59 -0500

    See the places

--- Pizza Finder/ViewController.swift
+++ Pizza Finder/ViewController.swift
@@ -47,8 +47,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         let search = MKLocalSearch(request: request)
         search.start { (response, error) in
             if let response = response{
-                for mapItem in response.mapItems{
-                    print(mapItem.name!)
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.name
+                    self.mapView.addAnnotation(annotation)
                 }
             }
         }
