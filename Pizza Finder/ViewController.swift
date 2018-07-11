8d7a4d9d93aa9ebe74d5faf184456207a44e9939
Rishi Anand <rishi@rishis-macbook-pro.local>
2018-07-11 13:24:09 -0500

    Zoom to Current Location

--- Pizza Finder/ViewController.swift
+++ Pizza Finder/ViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 import MapKit
 import CoreLocation
 
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    var region = MKCoordinateRegion()
+    
     
     let locManager = CLLocationManager()
     
@@ -21,12 +22,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         locManager.requestWhenInUseAuthorization()
         mapView.showsUserLocation = true
-        // Do any additional setup after loading the view, typically from a nib.
+        locManager.delegate = self
+        locManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first!
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let span = MKCoordinateSpanMake(0.025, 0.025)
+        region = MKCoordinateRegionMake(center, span)
+        mapView.setRegion(region, animated: true)
     }
 
 
