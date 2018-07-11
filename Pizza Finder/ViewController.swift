95ae5f6376228b07d64297d80cb8bd64be053cc5
Rishi Anand <rishi@rishis-macbook-pro.local>
2018-07-11 13:55:35 -0500

    info button moves to new view controller

--- Pizza Finder/ViewController.swift
+++ Pizza Finder/ViewController.swift
@@ -56,5 +56,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             }
         }
     }
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "pin")
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinView")
+            pinView?.canShowCallout = true
+            pinView?.rightCalloutAccessoryView = UIButton(type: .infoLight)
+        }
+        else {
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        performSegue(withIdentifier: "ShowLocationDetailsSegue", sender: nil)
+    }
 }
 
