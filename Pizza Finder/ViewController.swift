fc70af4327397d9783d349a6c45d55b5856aa8b5
Rishi Anand <rishi@rishis-macbook-pro.local>
2018-07-11 11:12:02 -0500

    MapViewAdded

--- Pizza Finder/ViewController.swift
+++ Pizza Finder/ViewController.swift
@@ -7,11 +7,20 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        locManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
