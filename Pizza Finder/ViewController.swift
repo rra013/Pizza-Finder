c0a356a953fa48e21a7bb939f58803441d5b8fdd
Rishi Anand <rishi@rishis-macbook-pro.local>
2018-07-11 13:32:23 -0500

    Map Queried and returns correct values

--- Pizza Finder/ViewController.swift
+++ Pizza Finder/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     var region = MKCoordinateRegion()
@@ -24,6 +24,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         mapView.showsUserLocation = true
         locManager.delegate = self
         locManager.startUpdatingLocation()
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,6 +40,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         mapView.setRegion(region, animated: true)
     }
 
-
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = "pizza"
+        request.region = region
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if let response = response{
+                for mapItem in response.mapItems{
+                    print(mapItem.name!)
+                }
+            }
+        }
+    }
 }
 
